bedf1989f4ad08384ebd209892482928987f04ef
WhatTheSlime <selim.lanouar@gmail.com>
2017-09-29 19:38:29 +0200

    Install Boostrap3 + Home Page

--- vapache/blog/views.py
+++ vapache/blog/views.py
@@ -7,10 +7,4 @@ from django.shortcuts import render
 
 def home(request):
 
-    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
-
-    text = """<h1>Bienvenue sur mon blog !</h1>
-
-              <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
-
-    return HttpResponse(text)
+    return render(request, 'blog/home.html')
