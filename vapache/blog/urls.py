ef1c8fccf7dafab648bfb4603643eef3490ebe3a
WhatTheSlime <selim.lanouar@gmail.com>
2017-11-16 13:12:54 +0100

    add article view

--- vapache/blog/urls.py
+++ vapache/blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     url(r'^index$', views.index),
     url(r'^accueil$', views.home, name='accueil'),
     url(r'^articles$', views.articles, name='articles'),
-    url(r'^article/(\d+)$', views.article, name='article')
+    url(r'^article/(\d+)$', views.article, name='article'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^addArticle$', views.addArticle, name='addArticle'),
+    url(r'^editArticle$', views.editArticle, name='editArticle'),
 
 ]
--- vapache/blog/views.py
+++ vapache/blog/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from blog.models import Article
+from .forms import ContactForm, ArticleForm
 
 # Create your views here.
 def index(request):
@@ -19,3 +20,42 @@ def article(request, id):
     """ Afficher un article complet """
     article = get_object_or_404(Article, id=id)
     return render(request, 'article.html', {'article': article})
+
+def contact(request):
+    # Construire le formulaire, soit avec les données postées,
+    # soit vide si l'utilisateur accède pour la première fois
+    # à la page.
+
+    form = ContactForm(request.POST or None)
+    # Nous vérifions que les données envoyées sont valides
+    # Cette méthode renvoie False s'il n'y a pas de données
+    # dans le formulaire ou qu'il contient des erreurs.
+    if form.is_valid():
+
+        # Ici nous pouvons traiter les données du formulaire
+        sujet = form.cleaned_data['sujet']
+        message = form.cleaned_data['message']
+        envoyeur = form.cleaned_data['envoyeur']
+        renvoi = form.cleaned_data['renvoi']
+
+        # Nous pourrions ici envoyer l'e-mail grâce aux données
+        # que nous venons de récupérer
+
+        envoi = True
+
+    # Quoiqu'il arrive, on affiche la page du formulaire.
+
+    return render(request, 'contact.html', locals())
+
+def addArticle(request):
+    form = ArticleForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return render(request, 'addArticle.html', locals())
+
+def editArticle(request, id):
+    #article = get_object_or_404(Article, id=id)
+    #form = ArticleForm(request.POST or None, instance=article)
+    #if form.is_valid():
+    #    form.save()
+    return render(request, 'editArticle.html', locals())
