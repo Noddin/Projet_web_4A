200ef5272e25a299a9cec543ba2f689ea75de498
WhatTheSlime <selim.lanouar@gmail.com>
2017-11-16 18:08:02 +0100

    Edit function + Buttons design

--- vapache/blog/urls.py
+++ vapache/blog/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     url(r'^accueil$', views.home, name='accueil'),
     url(r'^articles$', views.articles, name='articles'),
     url(r'^article/(\d+)$', views.article, name='article'),
-    url(r'^contact$', views.contact, name='contact'),
     url(r'^addArticle$', views.addArticle, name='addArticle'),
-    url(r'^editArticle$', views.editArticle, name='editArticle'),
-
+    url(r'^editArticle/(\d+)$', views.editArticle, name='editArticle')
 ]
--- vapache/blog/views.py
+++ vapache/blog/views.py
@@ -16,36 +16,10 @@ def articles(request):
     articles = Article.objects.all()
     return render(request, 'articles.html', {'derniers_articles': articles})
 
-def article(request, id):
+def article(request, titre):
     """ Afficher un article complet """
-    article = get_object_or_404(Article, id=id)
+    article = get_object_or_404(Article, titre=titre)
     return render(request, 'article.html', {'article': article})
-
-def contact(request):
-    # Construire le formulaire, soit avec les données postées,
-    # soit vide si l'utilisateur accède pour la première fois
-    # à la page.
-
-    form = ContactForm(request.POST or None)
-    # Nous vérifions que les données envoyées sont valides
-    # Cette méthode renvoie False s'il n'y a pas de données
-    # dans le formulaire ou qu'il contient des erreurs.
-    if form.is_valid():
-
-        # Ici nous pouvons traiter les données du formulaire
-        sujet = form.cleaned_data['sujet']
-        message = form.cleaned_data['message']
-        envoyeur = form.cleaned_data['envoyeur']
-        renvoi = form.cleaned_data['renvoi']
-
-        # Nous pourrions ici envoyer l'e-mail grâce aux données
-        # que nous venons de récupérer
-
-        envoi = True
-
-    # Quoiqu'il arrive, on affiche la page du formulaire.
-
-    return render(request, 'contact.html', locals())
 
 def addArticle(request):
     form = ArticleForm(request.POST or None)
@@ -54,8 +28,8 @@ def addArticle(request):
     return render(request, 'addArticle.html', locals())
 
 def editArticle(request, id):
-    #article = get_object_or_404(Article, id=id)
-    #form = ArticleForm(request.POST or None, instance=article)
-    #if form.is_valid():
-    #    form.save()
+    article = get_object_or_404(Article, id=id)
+    form = ArticleForm(request.POST or None, instance=article)
+    if form.is_valid():
+        form.save()
     return render(request, 'editArticle.html', locals())
